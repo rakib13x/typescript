8ff2752fdebf4184690d4760d68ded864da40f0a
rakib13x <rakib13x@gmail.com>
2024-04-29 09:22:04 +0600

    basic data type  of ts added

---
+++ level-2-course(prog-hero)/typescript-technocrat/module-1/src/1.4.ts
@@ -0,0 +1,25 @@
+//Basic Data types
+
+//string
+let firstName: string = "Rakib";
+
+//number
+let roll: number = 123;
+
+//boolean
+let isAdmin: boolean = true;
+
+//undefined
+let x: undefined = undefined;
+
+//null
+let y: null = null;
+
+//Array
+let friends: string[] = ["rachel", "monica"];
+let eligibleRollList: number[] = [1, 2, 3];
+
+//tuple ==> one type of array ==> order ==> type of values
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string] = [50, "Hello World"];
---
+++ level-2-course(prog-hero)/typescript-technocrat/module-1/src/index.ts
@@ -0,0 +1,2 @@
+let person: string = "Rakib Islam";
+console.log(person);
