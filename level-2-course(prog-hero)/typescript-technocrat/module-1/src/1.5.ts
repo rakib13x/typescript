c1734e531549904290b7b64b7c8a0481d60ac138
rakib13x <rakib13x@gmail.com>
2024-04-30 11:54:09 +0600

    Object module added

---
+++ level-2-course(prog-hero)/typescript-technocrat/module-1/src/1.5.ts
@@ -0,0 +1,12 @@
+// Reference Type ==> Object
+
+const user: {
+  readonly profession: "student"; // type ==> literal type
+  firstName: string;
+  middleName?: string; //optional type
+  lastName: string;
+} = {
+  profession: "student",
+  firstName: "Rakib",
+  lastName: "Islam",
+};
