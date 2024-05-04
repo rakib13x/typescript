e8f053e7d71f3b7764c047fd3454d56ca6bf15dd
rakib13x <rakib13x@gmail.com>
2024-05-04 13:56:11 +0600

    Generics  module added

---
+++ level-2-course(prog-hero)/typescript-technocrat/module-2/2.3(generics).ts
@@ -0,0 +1,30 @@
+{
+  /**---------------------------generics------------------------------------ */
+
+  type GenericArray<T> = Array<T>;
+
+  const rollNumbers: GenericArray<number> = [3, 4, 5, 6, 7];
+
+  const mentors: GenericArray<string> = ["Rakib", "Sakib"];
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "Rakib",
+      age: 29,
+    },
+    {
+      name: "Sakib",
+      age: 22,
+    },
+  ];
+
+  /**-----------------Generic  tuple---------------*/
+
+  type GenericTuple<X, Y> = [X, Y];
+  const man: GenericTuple<string, string> = ["Mr. X", "Mr. Y"];
+
+  const UserWithID: GenericTuple<number, { name: string; email: string }> = [
+    1234,
+    { name: "rakib", email: "rakib@gmail.com" },
+  ];
+}
