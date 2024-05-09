a940bebdeccc2720015578de4fb988b37223477f
rakib13x <rakib13x@gmail.com>
2024-05-09 16:36:33 +0600

    static module added

---
+++ level-2-course(prog-hero)/typescript-technocrat/module-3/3.7(static).ts
@@ -0,0 +1,18 @@
+{
+  //**Static */
+  class Counter {
+    static count: number = 0;
+
+    static increment() {
+      return (Counter.count += 1);
+    }
+    static decrement() {
+      return (Counter.count -= 1);
+    }
+  }
+
+  const instance1 = new Counter();
+  console.log(Counter.increment());
+  const instance2 = new Counter();
+  console.log(Counter.increment());
+}
